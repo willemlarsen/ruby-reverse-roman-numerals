7bd7fcc29858faedbe7fb44d3ee090feab394f98
Willem Larsen <willemlarsen@gmail.com>
2014-07-11 11:27:53 -0700

    refactored test to loop through hash

--- spec/reverse_roman_numerals_spec.rb
+++ spec/reverse_roman_numerals_spec.rb
@@ -2,12 +2,12 @@ require "rspec"
 require "reverse_roman_numerals.rb"
 
 describe "convert_roman_nums_back" do
-	context "converts Roman numerals back into regular numbers" do
-		it "converts I to 1" do
-			expect(convert_roman_nums_back("I")).to eq(1)
-		end
-		it "converts 5 to V" do
-			expect(convert_roman_nums_back("V")).to eq(5)
+	{
+		"I" => 1,
+		"V" => 5
+	}.each_pair do |roman, arabic|
+		it "converts roman numerals back to regular numbers" do
+		expect(convert_roman_nums_back(roman)).to eq(arabic)
 		end
 	end
 end
