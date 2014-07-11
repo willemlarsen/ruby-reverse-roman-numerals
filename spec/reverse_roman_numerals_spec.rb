f3f08bed5982e11f85dfa77723f0f6853df4b723
Willem Larsen <willemlarsen@gmail.com>
2014-07-11 13:08:08 -0700

    convert II to 2

--- spec/reverse_roman_numerals_spec.rb
+++ spec/reverse_roman_numerals_spec.rb
@@ -4,8 +4,8 @@ require "reverse_roman_numerals.rb"
 describe "convert_roman_nums_back" do
 	{
 		"I" => 1,
+		"II" => 2,
 		"V" => 5,
-		"II" => 2
 	}.each_pair do |roman, arabic|
 		it "converts roman numerals back to regular numbers" do
 		expect(convert_roman_nums_back(roman)).to eq(arabic)
