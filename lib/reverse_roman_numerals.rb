34fcb8154d8e9f6d1fe441cdb4161a31a7fa851d
Willem Larsen <willemlarsen@gmail.com>
2014-07-11 14:33:24 -0700

    extract counting_ones_and_fives method

--- lib/reverse_roman_numerals.rb
+++ lib/reverse_roman_numerals.rb
@@ -2,12 +2,18 @@ CONVERSIONS = {
 	"V" => 5,
 	"IV" => 4
 	}
+
+def counting_ones_and_fives(roman_num)
+	fives = roman_num.count("V") * 5
+	ones = roman_num.count("I")
+	arabic_num = fives + ones
+	arabic_num
+end
+
+
 def convert_roman_nums_back(roman_num)
 	if roman_num[-1] == "I"
-		fives = roman_num.count("V") * 5
-		ones = roman_num.count("I")
-		arabic_num = fives + ones
-		arabic_num
+		counting_ones_and_fives(roman_num)
 	else CONVERSIONS[roman_num]
 	end
 end
