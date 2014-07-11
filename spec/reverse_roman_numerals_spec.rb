9f5583530b201bf6e8cef6f21fc834c02be6b517
Willem Larsen <willemlarsen@gmail.com>
2014-07-11 14:00:08 -0700

    convert IV to 4

--- spec/reverse_roman_numerals_spec.rb
+++ spec/reverse_roman_numerals_spec.rb
@@ -6,6 +6,7 @@ describe "convert_roman_nums_back" do
 		"I" => 1,
 		"II" => 2,
 		"V" => 5,
+		"IV" => 4
 	}.each_pair do |roman, arabic|
 		it "converts roman numerals back to regular numbers" do
 		expect(convert_roman_nums_back(roman)).to eq(arabic)
